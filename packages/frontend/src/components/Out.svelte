0c7729dc916e7278eede9cf246637fc1081e661b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-04-25 03:03:31 +0900

    display out links better

---
+++ packages/frontend/src/components/Out.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a
+  class="text-blue-500 hover:underline after:content-['°']"
+  target="_blank"
+  rel="noreferrer"
+  {href}
+  {...$$restProps}
+>
+  {#if $$slots.default}
+    <slot />
+  {:else}
+    {href}
+  {/if}
+</a>
--- packages/frontend/src/components/ShelterCard.svelte
+++ packages/frontend/src/components/ShelterCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import type { Shelter } from "../../../../types";
+  import Out from "./Out.svelte";
 
   export let item: Shelter;
 </script>
@@ -7,13 +8,15 @@
 <div class="py-4">
   <h1 class="font-bold">{item.名稱}</h1>
   <h2>
-    <a
-      class="text-blue-500 hover:underline"
+    <Out
       title="於 Google 地圖檢視地址"
-      href={`https://www.google.com/maps/place/${item.地址}`}>{item.地址}</a
+      href={`https://www.google.com/maps/place/${item.地址}`}>{item.地址}</Out
     >
     <div>收容人數：{item.收容人數}人</div>
     <div>適用災害：{item.適用災害?.join("、")}</div>
     <div>備註：{item.備註 || ""}</div>
+    <div>
+      資料來源：<Out href={`https://data.gov.tw/dataset/${item.source}`}></Out>
+    </div>
   </h2>
 </div>
